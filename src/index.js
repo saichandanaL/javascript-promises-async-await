22cfd81416d7733196771a6e0b5c80801f359a9c
saichandanaL <saichandana2021@gmail.com>
2022-06-01 14:39:00 +0530

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import {fetchWithTimeout} from './services';
-import { fetchMovies } from './services';
-import { fetchBooks } from './services';
+import { result } from 'lodash';
+import {fetchWithTimeout ,fetchMovies , fetchBooks } from './services';
 
 // const movies = require('./data/movies.json');
 
+function getBooksAndMovies(){
+    return Promise.all([fetchBooks() , fetchMovies()])
+        .then(([books , movies])=>({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
+}
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results=>console.log('getBooksAndMoviesPromise', results));
