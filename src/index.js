ac2f9fd77d31f1e371f3de26659526c4bbeb93e6
saichandanaL <saichandana2021@gmail.com>
2022-06-01 14:11:17 +0530

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-//empty export function
+import {fetchWithTimeout} from './services.json'
+
+movies = require('./data/movies.json');
+
 export function fetchMovies(){
-    
+    const resolveFunction = ()=> movies;
+
+    fetchWithTimeout(1000)
+        .then(resolveFunction)
 }
+
+moviePromise = fetchMovies().then((results)=>{
+    console.log(results);
+})
